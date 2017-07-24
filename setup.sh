9991db2effbee1b703ca5fecc619599c48d6b97e
Kyle Arscott <arscotkm@mcmaster.ca>
2017-07-24 10:19:00 -1000

    Add file to setup either bbg or server for Beasley Neighborhood Association Weather Station. Add license.

---
+++ setup.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# This is the MAIN SCRIPT for downloading, setting up, and running the Beasley Weather Station 
+# on the BeagleBoneGreen or server.
+# This script will figure out which hardware you are running and either setup the BBG OR server
+
+# definitions
+USER=badger
+USER_HOME_DIR=/home/$USER
+USER_BEASLEY_DIR=$USER_HOME_DIR/beasley-weather-station/
+BBG_SCRIPTS_DIR=$USER_BEASLEY_DIR/bbg/acu/scripts
+SERVER_SCRIPTS_DIR=$USER_BEASLEY_DIR/server/scripts
+BBG_SETUP_SCRIPT=setupBBG.sh
+SERVER_SETUP_SCRIPT=setupServer.sh
+
+# exit as soon as there is an error
+set -e
+
+# step 1
+echo -e "\nCreating main user (badger) and file structure..."
+sudo mkdir -p $USER_BEASLEY_DIR
+if [ `id -u $USER` ]
+then
+	echo "User $USER already exists (this is good)"
+else
+	echo "If if prompts for password and Full name, make it \"$USER\" and press \"Enter\" for everything else."
+	sudo adduser --quiet $USER --home $USER_HOME_DIR
+	echo "Adding user $USER complete."
+fi
+
+# step 2
+echo -e "\nDownloading all necessary git repos for scripts and config files..."
+cd $USER_BEASLEY_DIR
+while true; do
+    read -p "Do you wish to reinstall git repos? [y/n]" yn
+    case $yn in
+        [Yy]* )
+        	read -p "Clear all existing repos in $USER folder? YES=[ENTER] or NO=CTRL-C"
+			sudo rm -rf $USER_BEASLEY_DIR/*
+			sudo git clone https://github.com/beasley-weather/server.git
+			sudo git clone https://github.com/beasley-weather/bbg.git
+			sudo git clone https://github.com/beasley-weather/data-generator.git
+			sudo git clone https://github.com/beasley-weather/extract-data.git
+			sudo git clone https://github.com/beasley-weather/project-tracking.git
+			sudo git clone https://github.com/beasley-weather/libdb.git
+			echo "Done downloading all beasley-weather-station repos." 
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo -e "\nRecursively change all file permissions to \"$USER\"..."
+sudo chown -R $USER:$USER $USER_HOME_DIR
+
+echo -e "\nCheck hardware architecture to see if I am running on a BeagleBoneGreen or server..."
+#if [ `arch` == "armhf" ]
+while true; do
+    read -p "Do you wish to setup the BeagleBone (b) or the Server (s)? [b/s]" bs
+    case $bs in
+        [Bb]* )
+			echo "Architecture is armhf, therefore this is a BEAGLE BONE GREEN/BLACK  and will be configured as such."
+			$BBG_SCRIPTS_DIR/$BBG_SETUP_SCRIPT
+			break;;
+        [Ss]* ) 
+			echo "Architecture is not armhf, therefore this is a SERVER and will be configured as such."
+			$SERVER_SCRIPTS_DIR/$SERVER_SETUP_SCRIPT
+			break;;
+        * ) echo "Please answer (b) or (s).";;
+    esac
+done
