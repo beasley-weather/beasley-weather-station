12258a73ac05309db89e1854f6909f129e59394a
francium <varunhooda1@gmail.com>
2017-10-12 21:55:36 -0400

    Fix permission for script command

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ while true; do
         [Yy]* )
         	read -p "Clear all existing repos in $USER folder? YES=[ENTER] or NO=CTRL-C"
 			sudo rm -rf $USER_BEASLEY_DIR
-			mkdir -p $USER_BEASLEY_DIR
+			sudo mkdir -p $USER_BEASLEY_DIR
 			cd $USER_BEASLEY_DIR
 			sudo git clone https://github.com/beasley-weather/beasley-weather-station.git # need to redownload self becase I just rm'ed myself
 			sudo git clone https://github.com/beasley-weather/server.git
